deb7583202e8e01777e75348db537d078a395ecf
Igor Clemente <igorelectrofest2011@hotmail.com>
2020-01-03 10:02:58 -0300

    BOOTCAMP_GOSTACK_MODULE12_STATEINITIALVALUETEST_CREATED_03012020

--- __tests__/src/store/reducers/techs.test.js
+++ __tests__/src/store/reducers/techs.test.js
@@ -5,6 +5,12 @@ import reducer, {
 import * as Techs from "../../../../src/store/modules/techs/actions";
 
 describe("Techs reducer", () => {
+  it("DEFAULT", () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it("ADD_TECH", () => {
     const state = reducer(INITIAL_STATE, Techs.addTech("Node.js"));
 
